2365f62fe1bea7a8cbd4b61db59edf311d8d385e
alexandr-fly-code <alexandr17world@gmail.com>
2019-02-19 20:14:00 +0200

    commit

--- src/components/Content/Experience.js
+++ src/components/Content/Experience.js
@@ -14,19 +14,19 @@ const Experience = () => {
             <SectionName name="Experience"/>
             <div className="wrap_colum_exp expirience_fix">
             
-                <div className="left_exp">
+                {/* <div className="left_exp">
                 {experience.map(el => 
                 <>
                     <p className="developer_position">{el.position}</p>
                     <p className="start_work">{el.workPeriod}</p>
                 </>
                 )} 
-                </div>
+                </div> */}
                 
                 <div className="right_exp">
                 {experience.map(el => 
                 <>
-                    <p className="company_name">{el.name}</p>
+                    <p className="company_name">{el.name} <span className="developer_position">{el.position} </span><p>({el.workPeriod})</p></p>
                     <ul>
                         {el.activity.map(item => <ol className="description">{item}</ol>)}
                     </ul>
